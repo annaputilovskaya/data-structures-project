9f0e65d86bf9daa1d9f6a66694437e178f22ffa7
putilovskaya <putilovskayaa@mail.ru>
2024-02-01 22:40:33 +0300

    fix stack.py

--- src/stack.py
+++ src/stack.py
@@ -1,13 +1,15 @@
 class Node:
     """Класс для узла стека"""
 
-    def __init__(self, data, next_node):
+    def __init__(self, data, next_node=None):
         """
         Конструктор класса Node
 
+
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Stack:
@@ -15,20 +17,25 @@ class Stack:
 
     def __init__(self):
         """Конструктор класса Stack"""
-        pass
+        self.top = None
 
     def push(self, data):
         """
         Метод для добавления элемента на вершину стека
 
+
         :param data: данные, которые будут добавлены на вершину стека
         """
-        pass
+        new_node = Node(data, self.top)
+        self.top = new_node
 
     def pop(self):
         """
         Метод для удаления элемента с вершины стека и его возвращения
 
+
         :return: данные удаленного элемента
         """
-        pass
+        top_node = self.top
+        self.top = top_node.next_node
+        return top_node.data
