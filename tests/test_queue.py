f6f598ea56ad26e1fc714232961b8e7b900025f1
putilovskaya <putilovskayaa@mail.ru>
2024-02-02 18:13:27 +0300

    add test_str to test_queue.py

--- tests/test_queue.py
+++ tests/test_queue.py
@@ -11,7 +11,7 @@ class TestQueue(unittest.TestCase):
         queue.enqueue('data3')
 
         self.assertEqual(queue.head.data, 'data1')
-        self.assertEqual(queue.head.next_node.data,'data2')
+        self.assertEqual(queue.head.next_node.data, 'data2')
         self.assertEqual(queue.tail.data, 'data3')
         self.assertEqual(queue.tail.next_node, None)
 
@@ -28,3 +28,12 @@ class TestQueue(unittest.TestCase):
         self.assertEqual(queue.dequeue(), 'data2')
         self.assertEqual(queue.dequeue(), 'data3')
         self.assertEqual(queue.dequeue(), None)
+
+    def test_str(self):
+        queue = Queue()
+        self.assertEqual(str(queue),  "")
+
+        queue.enqueue('data1')
+        queue.enqueue('data2')
+        queue.enqueue('data3')
+        self.assertEqual(str(queue), "data1\ndata2\ndata3")
