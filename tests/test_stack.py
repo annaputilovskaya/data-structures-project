31ac76b616290d9b3d39d7e5f7575e646c4e99b6
putilovskaya <putilovskayaa@mail.ru>
2024-02-10 22:59:59 +0300

    fix test_stack.py

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -4,14 +4,6 @@ import unittest
 from src.stack import Node, Stack
 
 
-class TestNode(unittest.TestCase):
-    def test_initial_node(self):
-        node1 = Node(5, Node)
-        node2 = Node('a', node1)
-        self.assertEqual(node1.data, 5)
-        self.assertEqual(node2.next_node, node1)
-
-
 class TestStack(unittest.TestCase):
     def test_push(self):
         stack1 = Stack()
